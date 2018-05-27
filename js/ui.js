6a186afd8a8ea24b358a7c79f5bd07d2cacb78d5
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-05-27 18:42:07 -0400

    added the options for the category from the api

--- js/ui.js
+++ js/ui.js
@@ -7,8 +7,22 @@ class UI {
     this.printCategories();
   }
   printCategories() {
-    const categoriesList = eventbrite.getCategoriesAPI().then(categories => {
-      console.log(categories);
-    });
+    const categoriesList = eventbrite
+      .getCategoriesAPI()
+      .then(categories => {
+        const categoriesList = categories.categories.categories;
+        const categorySelect = document.getElementById("category");
+
+        //   iterating over the array
+        categoriesList.forEach(category => {
+          const option = document.createElement("option");
+          option.value = category.id;
+          option.appendChild(document.createTextNode(category.name));
+          categorySelect.appendChild(option);
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
