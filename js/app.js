cd5bf0f16b1244676195ac70f60b910aadd62b86
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-05-27 19:10:53 -0400

    events displaying

--- js/app.js
+++ js/app.js
@@ -11,6 +11,19 @@ document.getElementById("submitBtn").addEventListener("click", e => {
   const category = document.getElementById("category").value;
   if (eventName !== "") {
     // query event brite api
+    eventbrite.queryAPI(eventName, category).then(events => {
+      // checking the events
+      const eventsList = events.events.events;
+      if (eventsList.length > 0) {
+        // printing the results
+      } else {
+        // message
+        ui.printMessage(
+          "Add an event or city",
+          "text-center alert alert-danger mt-4"
+        );
+      }
+    });
   } else {
     //   print error message
     ui.printMessage(
--- js/eventbrite.js
+++ js/eventbrite.js
@@ -2,6 +2,20 @@ class EventBrite {
   // constructor
   constructor() {
     this.token_auth = "ZXUTNKIMCF2E5IX3J3G5";
+    this.orderby = "date";
+  }
+
+  //   get the events from teh api
+  async queryAPI(eventName, category) {
+    const eventsResponse = await fetch(
+      `https://www.eventbriteapi.com/v3/events/search/?q=${eventName}&sort_by=${
+        this.orderby
+      }&categories=${category}&token=${this.token_auth}`
+    );
+    const events = await eventsResponse.json();
+    return {
+      events
+    };
   }
 
   //   getting the categories from the api
